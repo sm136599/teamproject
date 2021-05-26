716a5b68b467b6a9e1a22ec72f88ddbc60ead968
whitecloud <74234978+sm136599@users.noreply.github.com>
2021-05-26 23:12:06 +0900

    학생 수 반환, 학생 배열 반환

--- teamproject/Lecture.cpp
+++ teamproject/Lecture.cpp
@@ -7,31 +7,35 @@
 using namespace std;
 
 
-Lecture::Lecture(const char* name, const char* lecturer, const char* room, int code, double time, int limited)
-	: lectureCode(code), lectureTime(time), limitedNum(limited), studentCnt(0)
+Lecture::Lecture(const char* name, const char* lecturer, const char* room, int code, char* time, int limited)
+	: lectureCode(code), limitedNum(limited), studentCnt(0)
 {
 	for (int i = 0;i < MAXLIMITED;i++)
 	{
 		student[i] = 0;
 	}
+	this->lectureTime = new char[strlen(time) + 1];
 	this->lectureName = new char[strlen(name) + 1];
 	this->lecturer = new char[strlen(lecturer) + 1];
 	this->lectureRoom = new char[strlen(room) + 1];
+	strcpy(this->lectureTime, time);
 	strcpy(this->lectureName, name);
 	strcpy(this->lecturer, lecturer);
 	strcpy(this->lectureRoom, room);
 }
 
 Lecture::Lecture(Lecture& ref)
-	: lectureCode(ref.lectureCode), lectureTime(ref.lectureTime), limitedNum(ref.limitedNum), studentCnt(ref.studentCnt)
+	: lectureCode(ref.lectureCode), limitedNum(ref.limitedNum), studentCnt(ref.studentCnt)
 {
 	for (int i = 0;i < MAXLIMITED;i++)
 	{
 		student[i] = ref.student[i];
 	}
+	this->lectureTime = new char[strlen(ref.lectureTime) + 1];
 	this->lectureName = new char[strlen(ref.lectureName) + 1];
 	this->lecturer = new char[strlen(ref.lecturer) + 1];
 	this->lectureRoom = new char[strlen(ref.lectureRoom) + 1];
+	strcpy(this->lectureTime, ref.lectureTime);
 	strcpy(this->lectureName, ref.lectureName);
 	strcpy(this->lecturer, ref.lecturer);
 	strcpy(this->lectureRoom, ref.lectureRoom);
@@ -52,9 +56,21 @@ char* Lecture::GetLecturer() const
 	return lecturer;
 }
 
-void Lecture::ChangeLectureTime(double changedtime)
+int* Lecture::GetStudentID()
 {
-	lectureTime = changedtime;
+	return student;
+}
+
+int Lecture::GetStudentCnt() const
+{
+	return studentCnt;
+}
+
+void Lecture::ChangeLectureTime(char* changedtime)
+{
+	delete[]lectureTime;
+	this->lectureTime = new char[strlen(changedtime) + 1];
+	strcpy(this->lectureTime, changedtime);
 }
 void Lecture::ChangeLectureCode(int changedcode)
 {
@@ -83,30 +99,31 @@ void Lecture::ChangeLectureRoom(char* changedroom)
 	strcpy(this->lectureRoom, changedroom);
 }
 
-void Lecture::AddStudent(int studentId)
+bool Lecture::AddStudent(int studentId)
 {
 	if (studentCnt >= limitedNum)
 	{
 		cout << "수강인원이 꽉 찼습니다." << endl;
-		return;
+		return false;
 	}
 	else
 	{
 		student[studentCnt++] = studentId;
+		return true;
 	}
 }
 
 void Lecture::LectureAllInfoPrint() const
 {
-	cout << lectureCode << endl;
-	cout << lectureName << endl;
-	cout << lectureTime << endl;
-	cout << limitedNum << endl;
-	cout << lecturer << endl;
-	cout << lectureRoom << endl;
+	cout << "--------------------------" << endl;
+	cout << "강의코드: " << lectureCode << endl;
+	cout << "강의명: " << lectureName << endl;
+	cout << "강의시간: " << lectureTime << endl;
+	cout << "제한인원: " << studentCnt << "/" << limitedNum << endl;
+	cout << "교수명: " << lecturer << endl;
+	cout << "강의실: " << lectureRoom << endl;
+	cout << "--------------------------" << endl;
 }
-
-
 
 Lecture::~Lecture()
 {
--- teamproject/Lecture.h
+++ teamproject/Lecture.h
@@ -8,28 +8,30 @@
 class Lecture
 {
 private:
-	double lectureTime;				// 강의시간
 	int lectureCode;				// 강의코드
 	int limitedNum;					// 제한인원	
 	int studentCnt;					// 학생 수
-	int student[MAXLIMITED];		// 학생ID   
+	int student[MAXLIMITED];		// 학생ID
+	char* lectureTime;				// 강의시간
 	char* lectureName;				// 강의이름
 	char* lecturer;					// 담당교수명
 	char* lectureRoom;				// 강의실
 public:
-	Lecture(const char* name, const char* lecturer, const char* room, int code, double time, int limited);
+	Lecture(const char* name, const char* lecturer, const char* room, int code, char* time, int limited);
 	Lecture(Lecture& ref);
-	char* GetLectureName() const;
-	int GetLectureCode() const;
-	char* GetLecturer() const;
-	void ChangeLectureTime(double changedtime);
+	char* GetLectureName() const;					// 강의명 반환
+	int GetLectureCode() const;						// 강의코드 반환
+	char* GetLecturer() const;						// 교수명 반환
+	int* GetStudentID();							// 학생 배열 반환
+	int GetStudentCnt() const;
+	void ChangeLectureTime(char* changedtime);		
 	void ChangeLectureCode(int changedcode);
 	void ChangeLimitedNum(int changedlimited);
 	void ChangeLectureName(char* changedname);
 	void ChangeLecturer(char* changedlecturer);
-	void ChangeLectureRoom(char* changedroom);
-	void AddStudent(int studentId);
-	void LectureAllInfoPrint() const;
+	void ChangeLectureRoom(char* changedroom);		// 정보 변경 
+	bool AddStudent(int studentId);					// 학생추가 (학생이 수강신청할 때 불릴 함수) 제한인원을 넘기면 false반환 안넘기면 true 반환
+	void LectureAllInfoPrint() const;				// 강의 정보 출력
 	~Lecture();
 };
 
